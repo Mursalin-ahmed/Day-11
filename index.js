814967b9ab180ea1dbb488bd0dd8eec51f81fba4
Mursalin ahmed <mursalinpranto1342@gmail.com>
2022-11-10 01:53:30 +0600

    fs module complete

--- index.js
+++ index.js
@@ -3,19 +3,81 @@ var http=require('http');
 var server = http.createServer(function(req,res){
 
     if(req.url='/'){
-        //syncronus write
-        let error = fs.writeFileSync('demoSync.txt','Welcome to demo');
-        if(error)
-            {
-                res.writeHead(200,{'Content-Type':'text/html'});
-                res.write("File write fail");
-                res.end();
-            }
-            else{
-                res.writeHead(200,{'Content-Type':'text/html'});
-                res.write("File write Success");
-                res.end();
-            }
+        //Exist Syncronus
+        let result = fs.existsSync('ahome.html')
+        if(result){
+            res.end("true");
+        }
+        else{
+            res.end("false");
+        }
+        //Delete Syncronus
+        // let error = fs.unlinkSync('demoNewNewSync.txt')
+        // if(error)
+        //     {
+        //         res.writeHead(200,{'Content-Type':'text/html'});
+        //         res.write("File Delate fail");
+        //         res.end();
+        //     }
+        //     else{
+        //         res.writeHead(200,{'Content-Type':'text/html'});
+        //         res.write("File Delate Success");
+        //         res.end();
+        //     }   
+        
+        //Delate Asyncronus
+        // fs.unlink('demoNewNew.txt',function(error){
+        //      if(error)
+        //     {
+        //         res.writeHead(200,{'Content-Type':'text/html'});
+        //         res.write("File Delate fail");
+        //         res.end();
+        //     }
+        //     else{
+        //         res.writeHead(200,{'Content-Type':'text/html'});
+        //         res.write("File Delate Success");
+        //         res.end();
+        //     }   
+        // })
+        //Rename Syncronus
+        // let error = fs.renameSync('demoSync.txt','demoNewNewSync.txt');
+        // if(error)
+        //     {
+        //         res.writeHead(200,{'Content-Type':'text/html'});
+        //         res.write("File Rename fail");
+        //         res.end();
+        //     }
+        //     else{
+        //         res.writeHead(200,{'Content-Type':'text/html'});
+        //         res.write("File Rename Success");
+        //         res.end();
+        //     }
+        //Rename Asyncronus
+        // fs.rename('demoNew.txt','demoNewNew.txt',function(error){
+        //     if(error){
+        //         res.writeHead(200,{'Content-Type':'text/html'});
+        //         res.write("File Rename fail");
+        //         res.end();
+        //     }
+        //     else{
+        //         res.writeHead(200,{'Content-Type':'text/html'});
+        //         res.write("File Rename Success");
+        //         res.end();
+        //     }
+        // })
+        // //syncronus write
+        // let error = fs.writeFileSync('demoSync.txt','Welcome to demo');
+        // if(error)
+        //     {
+        //         res.writeHead(200,{'Content-Type':'text/html'});
+        //         res.write("File write fail");
+        //         res.end();
+        //     }
+        //     else{
+        //         res.writeHead(200,{'Content-Type':'text/html'});
+        //         res.write("File write Success");
+        //         res.end();
+        //     }
         //Asyncronus write
         // fs.writeFile('demo.txt','Hellow world',function(error){
         //     if(error)
