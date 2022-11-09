5bb0555e9f9f0734712f0cd6c982348e8ea7dfcb
Mursalin ahmed <mursalinpranto1342@gmail.com>
2022-11-10 01:02:17 +0600

    file write

--- index.js
+++ index.js
@@ -3,19 +3,49 @@ var http=require('http');
 var server = http.createServer(function(req,res){
 
     if(req.url='/'){
-        //Asyncronus
-        
+        //syncronus write
+        let error = fs.writeFileSync('demoSync.txt','Welcome to demo');
+        if(error)
+            {
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.write("File write fail");
+                res.end();
+            }
+            else{
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.write("File write Success");
+                res.end();
+            }
+        //Asyncronus write
+        // fs.writeFile('demo.txt','Hellow world',function(error){
+        //     if(error)
+        //     {
+        //         res.writeHead(200,{'Content-Type':'text/html'});
+        //         res.write("File write fail");
+        //         res.end();
+        //     }
+        //     else{
+        //         res.writeHead(200,{'Content-Type':'text/html'});
+        //         res.write("File write Success");
+        //         res.end();
+        //     }
+
+        // });
+
+
+
+        //Asyncronus-->read
         // fs.readFile('Home.html', function(error,data){
 
             // res.writeHead(200,{'Content-Type':'text/html'});
             // res.write(data);
             // res.end();
         // })
-        //Syncronus
-        let myData = fs.readFileSync('Home.html');
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.write(myData);
-        res.end();
+        //Syncronus-->read
+        // let myData = fs.readFileSync('Home.html');
+        // res.writeHead(200,{'Content-Type':'text/html'});
+        // res.write(myData);
+        // res.end();
 
     }
 });
